b37328e75c2592aa754ec8ac53d9e11345056972
abarax <leigh.appel@gmail.com>
2013-03-12 15:17:56 +1000

    Update 20130304.py

--- 20130304.py
+++ 20130304.py
@@ -1,12 +1,21 @@
-__author__ = 'abarax'
+__author__ = 'appell'
 
 
 input = 7
 
+history = {}
+
 def count_coins(denomination):
-    if (denomination == 0):
+    if (history.has_key(denomination)):
+        print "re-using ", denomination, " result is ", history[denomination]
+        return history[denomination]
+    elif (denomination == 0):
         return 1
     else:
-        return count_coins(denomination/3) + count_coins(denomination/2) + count_coins(denomination/4)
+        ret_val = count_coins(denomination/3) + count_coins(denomination/2) + count_coins(denomination/4)
+        history[denomination] = ret_val
+        return ret_val
 
 print(count_coins(input))
+
+print history
