551e2504633669237c6beb48fc643922fcafb347
abarax <leigh.appel@gmail.com>
2013-03-15 10:57:11 +1000

    Update 20130304.py
    Clean up.

--- 20130304.py
+++ 20130304.py
@@ -1,21 +1,15 @@
-__author__ = 'appell'
+input = input("Enter your coin coin_value: ")
 
+cache = {}
 
-input = 7
-
-history = {}
-
-def count_coins(denomination):
-    if (history.has_key(denomination)):
-        print "re-using ", denomination, " result is ", history[denomination]
-        return history[denomination]
-    elif (denomination == 0):
+def count_zero_coin_value_coins(coin_value):
+    if coin_value in cache:
+        return cache[coin_value]
+    elif (coin_value == 0):
         return 1
     else:
-        ret_val = count_coins(denomination/3) + count_coins(denomination/2) + count_coins(denomination/4)
-        history[denomination] = ret_val
+        ret_val = count_zero_coin_value_coins(coin_value/4) + count_zero_coin_value_coins(coin_value/3) + count_zero_coin_value_coins(coin_value/2)
+        cache[coin_value] = ret_val
         return ret_val
 
-print(count_coins(input))
-
-print history
+print "Total zero coin_value coins obtainable is: ", count_zero_coin_value_coins(input)
